5245ebbfa1b0166dce02d3692c7d68cb21f1dabd
Louise <taylor.louise.ann@gmail.com>
2018-09-23 22:17:29 +0100

    Fetch people in space json

--- src/api/server.js
+++ src/api/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const fetch = require('node-fetch');
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(json => res.send(json))
+    .catch(e => {
+      res.status(503);
+      res.send(e.message);
+    })
 });
 
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) });
