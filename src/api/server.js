6dc1216ccc806f3301aced6b674090f88a2675e9
Louise <taylor.louise.ann@gmail.com>
2018-09-23 22:00:40 +0100

    Update server

--- src/api/server.js
+++ src/api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
+const PORT = process.env.PORT || 8000;
 const app = express();
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(8000, () => { console.log('Server is running') });
+app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) });
