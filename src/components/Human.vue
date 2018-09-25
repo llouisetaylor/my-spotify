65e33cd1933c769df5e77ddfda3013cb001868da
Louise <taylor.louise.ann@gmail.com>
2018-09-25 18:02:58 +0100

    Add human name label

--- src/components/Human.vue
+++ src/components/Human.vue
@@ -1,18 +1,17 @@
 <template>
-  <div
-    class="human"
-    :style="`animation: float ${Math.random() * 4 + 3}s ease-in-out infinite;`"
-  >
+  <div class="human">
     <button
       @click="openInfoBox(name)"
       :aria-label="`Read about ${name}`"
     >
-      <img
-        src="../assets/human-in-space.png"
-        alt=""
-        :style="{transform: `rotate(${rotation}deg)`}"
-      />
-      <span class="human__button-text">{{name}}</span>
+      {{name}}
+      <div :style="`animation: float ${Math.random() * 4 + 3}s ease-in-out infinite;`">
+        <img
+          src="../assets/human-in-space.png"
+          alt=""
+          :style="{transform: `rotate(${rotation}deg)`}"
+        />
+      </div>
     </button>
   </div>
 </template>
@@ -37,25 +36,24 @@ export default {
 
 <style lang="scss">
   .human {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     display: inline-block;
     position: relative;
     margin: 0 40px;
 
-    &__button-text {
-      display: none;
-
-      @media screen and (max-width:900px) {
-        display: inline;
-      }
-    }
-
     button {
-      margin-bottom: 1px;
       cursor: pointer;
-      background: transparent;
-      border: none;
+      margin-bottom: 1px;
       padding: 20px;
       box-sizing: border-box;
+
+      background: transparent;
+      border: none;
+      font-size: 16px;
+      font-weight: bold;
+      color: lighten(#c6e2ff, 20%);
 
       img {
         @media screen and (max-width:900px) {
@@ -66,14 +64,15 @@ export default {
       @media screen and (max-width:900px) {
         opacity: 0.85;
         background: #C6E2FF;
+        color: black;
         width: 100%;
         font-size: 26px;
+        animation: none !important;
       }
     }
 
     @media screen and (max-width: 900px) {
       margin: 0;
-      animation: none !important;
       width: 100%;
     }
   }
