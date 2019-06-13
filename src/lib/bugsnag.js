9564a544a75bced2192316fd3519a8981a457396
Louise Taylor <louise.taylor01@bbc.co.uk>
2019-06-13 13:34:06 +0100

    Snag bugs

---
+++ src/lib/bugsnag.js
@@ -0,0 +1,3 @@
+import bugsnag from '@bugsnag/js';
+const bugsnagClient = bugsnag('a435a9b7d409d128ac30148e254d77bb');
+export default bugsnagClient;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './views/App.vue'
+import Vue from 'vue';
+import bugsnagVue from '@bugsnag/plugin-vue';
+import App from './views/App.vue';
+import bugsnagClient from './lib/bugsnag';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+bugsnagClient.use(bugsnagVue, Vue);
 
 new Vue({
   render: h => h(App)
-}).$mount('#root')
+}).$mount('#root');
